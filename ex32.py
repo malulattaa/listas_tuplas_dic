6b05672a89054d98476596f5f03d264f5fc658c5
malulattaa <maria.latta2@estudante.ifms.edu.br>
2025-05-22 11:12:17 -0400

    ex 32

--- ex32.py
+++ ex32.py
@@ -3,4 +3,22 @@ Dadaumalista de nomes, retorne uma nova lista com os nomes em letras mai´ uscul
 """
 
 #fazer um len pra passar nas letra e por um lower na primeira
+lista_nome = []
 
+while True:
+    nome = input("Digite nomes para adicionar na lista ou (sair) para encerrar a execução: ").lower()
+    if nome == "sair":
+        break
+    lista_nome.append(nome)
+
+for i in lista_nome:
+    maiuscula = i.capitalize()
+    print(maiuscula)
+
+#sem o capitalize
+
+for i in lista_nome:
+    m = i[0].upper() + i[1:].lower() #1: pega o restante
+    print(m)
+    
+#acho que não precisaria do for aq
