4f90d8c20e89507dadb432ccec1eb2d95ace779c
malulattaa <maria.latta2@estudante.ifms.edu.br>
2025-06-25 15:34:41 -0400

    ex49

--- exercicios_lista80/ex49.py
+++ exercicios_lista80/ex49.py
@@ -5,9 +5,31 @@
 #len
 #lista[j], lista[j+1]
 
+#para a direita
+def girar(giro, lista):
+    if giro <= 0:
+        return lista
+    #recursividade, giro -1?
+    ultimo = lista[-1]
+    resto = lista[:-1]
+    return girar(giro -1, [ultimo] + resto)
+
 lista = [1, 2, 3, 4, 5, 6]
 
 giro = int(input("Deseja girar a lista para a direita quantas vezes? "))
+girada = girar(giro, lista)
+print(girada)
 
-def girar(lista):
-    pass
+#para esquerda
+def girar_esquerda(giro, lista):
+    if giro <= 0:
+        return lista
+    primeiro = lista[0]
+    resto = lista[1:]
+    return girar_esquerda(giro - 1, resto + [primeiro])
+
+lista = [1, 2, 3, 4, 5, 6]
+
+giro = int(input("Deseja girar a lista para a direita quantas vezes? "))
+girada = girar_esquerda(giro, lista)
+print(girada)
